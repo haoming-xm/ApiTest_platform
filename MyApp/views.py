eb6bf4759f8bfa2baa003e0bbf049694c3b92a06
minhao <15243143754@163.com>
2021-03-24 13:42:22 +0800

    first html

---
+++ MyApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+# Create your views here.
+
+def welcome(request):
+    print('进来了')
+    return render(request,'welcome.html')
